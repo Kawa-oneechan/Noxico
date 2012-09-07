c8666a2362eda8a5c53b084ac6d619653ffa11e5
Kawa <Kawa@localhost>
2012-09-07 17:10:56 +0200

    I told you about forgetting to add files, bro. I TOLD YOU DOG!

---
+++ Jint.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Jint;
+
+namespace Noxico
+{
+	public class Javascript
+	{
+		public static JintEngine MainMachine { get; set; }
+
+		public static JintEngine Create()
+		{
+			var jint = new JintEngine();
+			Ascertain(jint);
+			return jint;
+		}
+
+		public static void Ascertain(JintEngine jint, bool asBoardChars = false)
+		{
+			jint.SetFunction("eval", new Func<string, int>(x => 0));
+			if (NoxicoGame.HostForm.Noxico.Player != null)
+			{
+				if (asBoardChars)
+					jint.SetParameter("player", NoxicoGame.HostForm.Noxico.Player);
+				else
+					jint.SetParameter("player", NoxicoGame.HostForm.Noxico.Player.Character);
+			}
+			//TODO: predefine more stuff.
+			jint.SetParameter("Gender", typeof(Gender));
+			jint.SetParameter("MorphReport", typeof(MorphReportLevel));
+		}
+	}
+}
