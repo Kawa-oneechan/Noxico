84124537c1a33a3c6654bdc85726ac6c2c5a1550
Kawa <Kawa-oneechan@users.noreply.github.com>
2013-11-12 22:43:01 +0100

    Commit: ground now looks or at least can look as planned.

--- Character.cs
+++ Character.cs
@@ -881,9 +881,9 @@ namespace Noxico
 			filters["bodymatch"] = this.GetClosestBodyplanMatch();
 			var inventory = this.GetToken("items");
 			var clothing = new List<Token>();
-			clothing.AddRange(WorldGen.GetRandomLoot("npc", "underwear", filters));
-			clothing.AddRange(WorldGen.GetRandomLoot("npc", "clothing", filters));
-			clothing.AddRange(WorldGen.GetRandomLoot("npc", "accessories", filters));
+			clothing.AddRange(DungeonGenerator.GetRandomLoot("npc", "underwear", filters));
+			clothing.AddRange(DungeonGenerator.GetRandomLoot("npc", "clothing", filters));
+			clothing.AddRange(DungeonGenerator.GetRandomLoot("npc", "accessories", filters));
 			var check = new Func<Token, bool>(x =>
 			{
 				var ki = NoxicoGame.KnownItems.FirstOrDefault(i => i.ID == x.Name);
@@ -919,7 +919,7 @@ namespace Noxico
 					inventory.AddToken(item).AddToken("equipped");
 			}
 			var armedOne = false;
-			foreach (var item in WorldGen.GetRandomLoot("npc", "arms", filters))
+			foreach (var item in DungeonGenerator.GetRandomLoot("npc", "arms", filters))
 			{
 				var arm = inventory.AddToken(item);
 				if (!armedOne)
@@ -928,7 +928,7 @@ namespace Noxico
 					arm.AddToken("equipped");
 				}
 			}
-			foreach (var item in WorldGen.GetRandomLoot("npc", "food", filters))
+			foreach (var item in DungeonGenerator.GetRandomLoot("npc", "food", filters))
 				inventory.AddToken(item);
 
 			this.RemoveToken("lootset_id");
