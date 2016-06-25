d515c4da58dabdf428f82cc01b3b73ccd281df86
Kawa <Kawa-oneechan@users.noreply.github.com>
2016-06-25 21:59:36 +0200

    Commit: started on a bit of documentation.

--- Subscreens/ActionList.cs
+++ Subscreens/ActionList.cs
@@ -5,23 +5,38 @@ using System.Text;
 
 namespace Noxico
 {
+	/// <summary>
+	/// Displays a list of actions to choose from.
+	/// </summary>
 	public class ActionList
 	{
 		private static Action onChoice;
-		private static Dictionary<object, string> options;
+		private static Dictionary<object, string> options; //keys are used as values for Answer.
 		private static int option;
 
 		private static UIWindow win;
 		private static UIList lst;
 
+		/// <summary>
+		/// The chosen action.
+		/// </summary>
 		public static object Answer { get; private set; }
 
+		/// <summary>
+		/// Displays a list at the given screen location or close to it.
+		/// </summary>
+		/// <param name="title">The title of the window.</param>
+		/// <param name="x">The horizontal coordinate to aim the window at.</param>
+		/// <param name="y">The vertical coordinate to aim the window at.</param>
+		/// <param name="options">A list of options to display.</param>
+		/// <param name="okay">What to do when an option is chosen.</param>
 		public static void Show(string title, int x, int y, Dictionary<object, string> options, Action okay)
 		{
 			option = 0;
 			onChoice = okay;
 			ActionList.options = options;
 
+			//Determine window width according to its contents.
 			var width = title.Length() + 4;
 			foreach (var o in options.Values)
 			{
@@ -29,13 +44,17 @@ namespace Noxico
 					width = o.Length();
 			}
 			width += 4;
+			//Place the window just to the right of the specified location.
+			//If this goes off-screen, try placing it to the left instead.
 			if (x + 1 + width >= 100)
 				x = x - width;
 			else
 				x++;
 			var height = options.Count + 2;
+			//Check if we're going off the bottom of the screen and correct.
 			if (y + height >= 30)
 				y = 30 - height;
+			//If we go off the left or top, fuck it -- overlap the target.
 			if (x < 0)
 				x = 0;
 			if (y < 0)
@@ -49,7 +68,7 @@ namespace Noxico
 				option = lst.Index;
 				ActionList.Answer = options.Keys.ToArray()[option];
 			};
-			lst.Change(null, null);
+			lst.Change(null, null); //Make sure we have something -- the first item -- selected.
 			UIManager.Elements.Add(win);
 			UIManager.Elements.Add(lst);
 
@@ -58,6 +77,9 @@ namespace Noxico
 			NoxicoGame.Subscreen = ActionList.Handler;
 		}
 
+		/// <summary>
+		/// Generic Subscreen handler.
+		/// </summary>
 		public static void Handler()
 		{
 			if (Subscreens.FirstDraw)
@@ -73,13 +95,14 @@ namespace Noxico
 
 			if (NoxicoGame.IsKeyDown(KeyBinding.Back) || NoxicoGame.IsKeyDown(KeyBinding.Accept) || Vista.Triggers == XInputButtons.A || Vista.Triggers == XInputButtons.B)
 			{
+				//If we pressed Back/Esc or [B], pick the out-of-band -1 sentinel option.
 				if (NoxicoGame.IsKeyDown(KeyBinding.Back) || Vista.Triggers == XInputButtons.B)
 					option = -1;
 
 				Enter(null, null);
 
 				ActionList.Answer = option == -1 ? -1 : options.ElementAt(option).Key;
-				onChoice();
+				onChoice(); //Let the caller handle things.
 				NoxicoGame.ClearKeys();
 			}
 			else
