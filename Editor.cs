d689f4221d85976632a4c4c3794e7e108c9faf65
Kawa <Kawa-oneechan@users.noreply.github.com>
2017-01-18 17:26:18 +0100

    Commit: removed designer support from debug editor to allow conditionally leaving out the whole thing.

--- Editor.cs
+++ Editor.cs
@@ -1,47 +1,59 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Windows.Forms;
 using System.Drawing.Design;
+using System.Windows.Forms;
 using System.Windows.Forms.Design;
+using System.Collections.Generic;
 
 namespace Noxico
 {
-	public partial class Editor : Form
+#if DEBUG
+	public class Editor : Form
 	{
+		private ComboBox combo;
+		private TabControl tabs;
+
 		public Editor()
 		{
-			InitializeComponent();
+			Text = "Noxico Debug Editor 0.5";
+			ClientSize = new Size(388, 442);
+			tabs = new TabControl();
+			tabs.Dock = DockStyle.Fill;
+			Controls.Add(tabs);
+			combo = new ComboBox();
+			combo.DropDownStyle = ComboBoxStyle.DropDownList;
+			combo.Dock = DockStyle.Top;
+			combo.SelectedIndexChanged += new EventHandler(combo_SelectedIndexChanged);
+			Controls.Add(combo);
 		}
 
 		public void LoadBoard(Board board)
 		{
-			comboBox1.Items.Clear();
-			comboBox1.Items.AddRange(board.Entities.ToArray());
-			comboBox1.SelectedIndex = 0;
-			var host = (Form)NoxicoGame.HostForm;
+			combo.Items.Clear();
+			combo.Items.AddRange(board.Entities.ToArray());
+			combo.Items.Add(board);
+			combo.SelectedIndex = 0;
 			ShowDialog();
 		}
 
-		private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+		private void combo_SelectedIndexChanged(object sender, EventArgs e)
 		{
-			tabControl1.TabPages.Clear();
-			AddTab(comboBox1.SelectedItem);
-			comboBox1.Select();
+			tabs.TabPages.Clear();
+			AddTab(combo.SelectedItem);
+			combo.Select();
 		}
 
 		private void AddTab(object o)
 		{
 			var page = new TabPage(o.GetType().ToString());
 			var prop = new PropertyGrid();
-			page.Controls.Add(prop);
 			prop.Dock = DockStyle.Fill;
 			prop.SelectedObject = o;
-			tabControl1.TabPages.Add(page);
+			prop.ToolbarVisible = false;
+			prop.PropertySort = PropertySort.CategorizedAlphabetical;
+			page.Controls.Add(prop);
+			tabs.TabPages.Add(page);
 			if (o is BoardChar)
 				AddTab(((BoardChar)o).Character);
 			else if (o is Container)
@@ -56,7 +68,6 @@ namespace Noxico
 		private Bitmap sheet;
 		private Brush highlight;
 		public int Value { get; set; }
-
 
 		public GlyphSelectorForm()
 		{
@@ -87,8 +98,6 @@ namespace Noxico
 
 	public class GlyphSelector : UITypeEditor
 	{
-
-
 		public override object EditValue(ITypeDescriptorContext context, IServiceProvider provider, object value)
 		{
 			if (context != null && context.Instance != null && provider != null)
@@ -110,4 +119,64 @@ namespace Noxico
 			return UITypeEditorEditStyle.Modal;
 		}
 	}
+
+	public class TokenEditorForm : Form
+	{
+		public List<Token> Value { get; set; }
+		private TokenCarrier carrier;
+		private TextBox textBox;
+
+		public TokenEditorForm()
+		{
+			this.Text = "Token editor";
+			this.ClientSize = new Size(512, 512);
+
+			textBox = new TextBox();
+			textBox.Multiline = true;
+			textBox.Dock = DockStyle.Fill;
+			textBox.AcceptsTab = true;
+			textBox.ScrollBars = ScrollBars.Both;
+			Controls.Add(textBox);
+
+			Load += new EventHandler(TokenEditorForm_Load);
+			FormClosed += new FormClosedEventHandler(TokenEditorForm_FormClosed);
+		}
+
+		void TokenEditorForm_Load(object sender, EventArgs e)
+		{
+			carrier = new TokenCarrier();
+			textBox.Text = carrier.DumpTokens(Value, 0);
+		}
+
+		void TokenEditorForm_FormClosed(object sender, FormClosedEventArgs e)
+		{
+			carrier.Tokenize(textBox.Text);
+			Value = carrier.Tokens;
+		}
+	}
+
+	public class TokenEditor : UITypeEditor
+	{
+		public override object EditValue(ITypeDescriptorContext context, IServiceProvider provider, object value)
+		{
+			if (context != null && context.Instance != null && provider != null)
+			{
+				var edSvc = (IWindowsFormsEditorService)provider.GetService(typeof(IWindowsFormsEditorService));
+				if (edSvc != null)
+				{
+					var editor = new TokenEditorForm();
+					editor.Value = (List<Token>)Convert.ChangeType(value, context.PropertyDescriptor.PropertyType);
+					edSvc.ShowDialog(editor);
+					return editor.Value;
+				}
+			}
+			return value;
+		}
+
+		public override UITypeEditorEditStyle GetEditStyle(ITypeDescriptorContext context)
+		{
+			return UITypeEditorEditStyle.Modal;
+		}
+	}
+#endif
 }
