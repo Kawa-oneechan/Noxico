ef81f72f0579e090ddce72777842b636612e798f
Kawa <Kawa@localhost>
2017-04-03 20:10:22 +0200

    Commit: a bit more madlibs data

--- Game.cs
+++ Game.cs
@@ -231,8 +231,8 @@ namespace Noxico
 			test.DumpToHtml();
 			return;
 			*/
-			//var testChar = Character.Generate("felin", Gender.Female);
-			//var test1 = "[?:helloagain]".Viewpoint(testChar);
+			//var testChar = Character.Generate("felin", Gender.Male);
+			//var test1 = "[t:He] [?:gesture-t-flirty].".Viewpoint(testChar);
 
 			InGameTime = new DateTime(740 + Random.Next(0, 20), 6, 26, DateTime.Now.Hour, 0, 0); //InGameTime = new NoxicanDate(740 + Random.Next(0, 20), 6, 26, DateTime.Now.Hour, 0, 0);
 			TravelTargets = new Dictionary<int, string>();
